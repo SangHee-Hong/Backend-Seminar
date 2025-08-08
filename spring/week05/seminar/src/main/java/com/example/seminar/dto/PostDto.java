358ce82b7def4df479c5b38d4b0cbeed8dc97c05
SangHee-Hong <hongdabagi@gmail.com>
2025-08-09 03:27:20 +0900

    refactor : 애노테이션 사용

--- spring/week05/seminar/src/main/java/com/example/seminar/dto/PostDto.java
+++ spring/week05/seminar/src/main/java/com/example/seminar/dto/PostDto.java
@@ -1,5 +1,6 @@
 package com.example.seminar.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PostDto {
     private int id;
     private String title;
@@ -14,13 +16,6 @@ public class PostDto {
     private String writer;
     private int boardId;
 
-    public PostDto(int id, String title, String content, String writer, int boardId){
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.writer = writer;
-        this.boardId = boardId;
-    }
     @Override
     public String toString() {
         return "PostDto{"+"title="+title+'\''+
